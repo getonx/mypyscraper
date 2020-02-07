dd0dfd259933cb10637bd2c25613bc28de397038
getonx <jerzywicher@gmail.com>
2020-02-07 20:14:41 +0000

    nomsg

--- iterator.py
+++ iterator.py
@@ -5,9 +5,9 @@ class Iterator:
     def __init__(self, queue, browser):
         self.queue = queue
         self.visited = self.queue.visited
-        self.queue.add_to_queue({'url':'https://webscraper.io/test-sites/e-commerce/allinone'})
+        self.queue.add_to_queue({'url':'https://www.amazon.co.uk/'})
         self.browser = browser
-        
+
     def make_step(self):
         self.browser.get_page(self.queue.get_first())
 
